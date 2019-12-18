6910a05c268fbceeef000b7b2b5f9b7ee0fbb032
Gevorg <gevman97@gmail.com>
2019-12-18 15:37:45 +0400

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from flask_migrate import Migrate,MigrateCommand
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG']=True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://nnd7fl2rcgzmls5l:ex0isaorv23gefvw@thzz882efnak0xod.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/a9bwgfq691fo7jb9'
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 60
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+manager=Manager(app)
+manager.add_command('db',MigrateCommand)
+
---
+++ models.py
@@ -0,0 +1,32 @@
+from app import db,app
+from datetime import date,time,datetime
+
+
+class user(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(100),unique=True)
+    password=db.Column(db.String(200))
+    work_hours=db.Column(db.String(200))
+    start_date = db.Column(db.DateTime, default=datetime.now)
+    start_time = db.Column(db.String(200))
+    end_time = db.Column(db.String(200))
+    day = db.relationship('days',backref='username')
+
+    def __init__(self,*args,**kwargs):
+        super(user,self).__init__(*args,**kwargs)
+
+    def __repr__(self):
+        return self.username
+
+
+class days(db.Model):
+    id=db.Column(db.Integer,primary_key = True)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
+    day =db.Column(db.String(200))
+    work_hours = db.Column(db.String(200))
+
+    def __init__(self,*args,**kwargs):
+        super(days,self).__init__(*args,**kwargs)
+
+    def __repr__(self):
+        return self.day
